eedc6be7ade1c48682e0341114820edb85ce1dde
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-01 23:33:50 -0600

    Add counter display

--- src/Components/ScoreBoard/ScoreBoard.js
+++ src/Components/ScoreBoard/ScoreBoard.js
@@ -5,7 +5,10 @@ const ScoreBoard = props => {
 
   return (
     <div className="scoreboard-outer-container">
-      <h3 className='round-display'>ROUND: {props.roundCount}</h3>
+      <div className='round-moves-container'>
+        <h3 className='round-display'>ROUND: {props.roundCount}</h3>
+        <h3 className='round-display'>MOVES: {props.playerClickDisplay}/{props.roundCount}</h3>
+      </div>
       <button onClick={props.startGame} className='new-game-button'>NEW GAME</button>
     </div>
   );
