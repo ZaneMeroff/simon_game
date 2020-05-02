d1fa35b991026435187337768a952d029034c73c
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-02 01:21:44 -0600

    Merge pull request #8 from ZaneMeroff/bug-fixes
    Bug fixes

--- src/Components/GameBoard/GameBoard.js
+++ src/Components/GameBoard/GameBoard.js
@@ -17,6 +17,7 @@ const GameBoard = () => {
   const [myOrYours, setMyOrYours] = useState('');
   const [turnText, setTurnText] = useState('');
   const [playerClickDisplay, setPlayerClickDisplay] = useState(0);
+  const [disableButtons, setDisableButtons] = useState(true);
 
   const colorKey = {
     0 : setRedActive,
@@ -38,12 +39,12 @@ const GameBoard = () => {
   }
 
   const validateAnswer = () => {
-    console.log('random num array: ', randomNumArray);
-    console.log('playerResponse: ', playerResponse);
+    // console.log('random num array: ', randomNumArray);
+    // console.log('playerResponse: ', playerResponse);
     if (roundCount === 1 && playerResponse[0] === randomNumArray[0]) {
       setMyOrYours('RIGHT!')
       setTurnText('')
-      // increaseRoundCount();
+      setDisableButtons(true)
       playerResponse = []
       setTimeout(() => {
         startRound2();
@@ -53,7 +54,7 @@ const GameBoard = () => {
     if (roundCount === 2 && playerResponse[0] === randomNumArray[0] && playerResponse[1] === randomNumArray[1]) {
       setMyOrYours('RIGHT!')
       setTurnText('')
-      // increaseRoundCount();
+      setDisableButtons(true)
       playerResponse = []
       setTimeout(() => {
         startRound3();
@@ -63,7 +64,7 @@ const GameBoard = () => {
     if (roundCount === 3 && playerResponse[0] === randomNumArray[0] && playerResponse[1] === randomNumArray[1] && playerResponse[2] === randomNumArray[2]) {
       setMyOrYours('RIGHT!')
       setTurnText('')
-      // increaseRoundCount();
+      setDisableButtons(true)
       playerResponse = []
       setTimeout(() => {
         startRound4();
@@ -73,7 +74,7 @@ const GameBoard = () => {
     if (roundCount === 4 && playerResponse[0] === randomNumArray[0] && playerResponse[1] === randomNumArray[1] && playerResponse[2] === randomNumArray[2] && playerResponse[3] === randomNumArray[3]) {
       setMyOrYours('RIGHT!')
       setTurnText('')
-      // increaseRoundCount();
+      setDisableButtons(true)
       playerResponse = []
       setTimeout(() => {
         startRound5();
@@ -83,7 +84,7 @@ const GameBoard = () => {
     if (roundCount === 5 && playerResponse[0] === randomNumArray[0] && playerResponse[1] === randomNumArray[1] && playerResponse[2] === randomNumArray[2] && playerResponse[3] === randomNumArray[3] && playerResponse[4] === randomNumArray[4]) {
       setMyOrYours('RIGHT!')
       setTurnText('')
-      // increaseRoundCount();
+      setDisableButtons(true)
       playerResponse = []
       setTimeout(() => {
         startRound6();
@@ -93,7 +94,7 @@ const GameBoard = () => {
     if (roundCount === 6 && playerResponse[0] === randomNumArray[0] && playerResponse[1] === randomNumArray[1] && playerResponse[2] === randomNumArray[2] && playerResponse[3] === randomNumArray[3] && playerResponse[4] === randomNumArray[4] && playerResponse[5] === randomNumArray[5]) {
       setMyOrYours('RIGHT!')
       setTurnText('')
-      // increaseRoundCount();
+      setDisableButtons(true)
       playerResponse = []
       setTimeout(() => {
         startRound7();
@@ -103,7 +104,7 @@ const GameBoard = () => {
     if (roundCount === 7 && playerResponse[0] === randomNumArray[0] && playerResponse[1] === randomNumArray[1] && playerResponse[2] === randomNumArray[2] && playerResponse[3] === randomNumArray[3] && playerResponse[4] === randomNumArray[4] && playerResponse[5] === randomNumArray[5] && playerResponse[6] === randomNumArray[6]) {
       setMyOrYours('RIGHT!')
       setTurnText('')
-      // increaseRoundCount();
+      setDisableButtons(true)
       playerResponse = []
       setTimeout(() => {
         startRound8();
@@ -113,7 +114,7 @@ const GameBoard = () => {
     if (roundCount === 8 && playerResponse[0] === randomNumArray[0] && playerResponse[1] === randomNumArray[1] && playerResponse[2] === randomNumArray[2] && playerResponse[3] === randomNumArray[3] && playerResponse[4] === randomNumArray[4] && playerResponse[5] === randomNumArray[5] && playerResponse[6] === randomNumArray[6] && playerResponse[7] === randomNumArray[7]) {
       setMyOrYours('RIGHT!')
       setTurnText('')
-      // increaseRoundCount();
+      setDisableButtons(true)
       playerResponse = []
       setTimeout(() => {
         startRound9();
@@ -123,7 +124,7 @@ const GameBoard = () => {
     if (roundCount === 9 && playerResponse[0] === randomNumArray[0] && playerResponse[1] === randomNumArray[1] && playerResponse[2] === randomNumArray[2] && playerResponse[3] === randomNumArray[3] && playerResponse[4] === randomNumArray[4] && playerResponse[5] === randomNumArray[5] && playerResponse[6] === randomNumArray[6] && playerResponse[7] === randomNumArray[7] && playerResponse[8] === randomNumArray[8]) {
       setMyOrYours('RIGHT!')
       setTurnText('')
-      // increaseRoundCount();
+      setDisableButtons(true)
       playerResponse = []
       setTimeout(() => {
         startRound10();
@@ -133,6 +134,7 @@ const GameBoard = () => {
     if (roundCount === 10 && playerResponse[0] === randomNumArray[0] && playerResponse[1] === randomNumArray[1] && playerResponse[2] === randomNumArray[2] && playerResponse[3] === randomNumArray[3] && playerResponse[4] === randomNumArray[4] && playerResponse[5] === randomNumArray[5] && playerResponse[6] === randomNumArray[6] && playerResponse[7] === randomNumArray[7] && playerResponse[8] === randomNumArray[8] && playerResponse[9] === randomNumArray[9]) {
       setMyOrYours('RIGHT!')
       playerResponse = []
+      setDisableButtons(true)
       setTimeout(() => {
         setMyOrYours('YOU')
         setTurnText('WIN!')
@@ -141,19 +143,19 @@ const GameBoard = () => {
     } else {
       setMyOrYours('GAME')
       setTurnText('OVER!')
+      setDisableButtons(true)
     }
   }
 
   const startRound1 = () => {
-    if (roundCount === 1) {
-      setTimeout(() => {
-        colorKey[randomNumArray[0]](true)
-      }, 800)
-      setTimeout(() => {
-        colorKey[randomNumArray[0]](false)
-        setMyOrYours('YOUR')
-      }, 1600)
-    }
+    setTimeout(() => {
+      colorKey[randomNumArray[0]](true)
+    }, 800)
+    setTimeout(() => {
+      colorKey[randomNumArray[0]](false)
+      setMyOrYours('YOUR')
+      setDisableButtons(false)
+    }, 1600)
   }
 
   const startRound2 = () => {
@@ -174,6 +176,7 @@ const GameBoard = () => {
     setTimeout(() => {
       colorKey[randomNumArray[1]](false)
       setMyOrYours('YOUR')
+      setDisableButtons(false)
     }, 3200)
   }
 
@@ -201,6 +204,7 @@ const GameBoard = () => {
     setTimeout(() => {
       colorKey[randomNumArray[2]](false)
       setMyOrYours('YOUR')
+      setDisableButtons(false)
     }, 4800)
   }
 
@@ -234,6 +238,7 @@ const GameBoard = () => {
     setTimeout(() => {
       colorKey[randomNumArray[3]](false)
       setMyOrYours('YOUR')
+      setDisableButtons(false)
     }, 6400)
   }
 
@@ -273,6 +278,7 @@ const GameBoard = () => {
     setTimeout(() => {
       colorKey[randomNumArray[4]](false)
       setMyOrYours('YOUR')
+      setDisableButtons(false)
     }, 8000)
   }
 
@@ -318,6 +324,7 @@ const GameBoard = () => {
     setTimeout(() => {
       colorKey[randomNumArray[5]](false)
       setMyOrYours('YOUR')
+      setDisableButtons(false)
     }, 9600)
   }
 
@@ -369,6 +376,7 @@ const GameBoard = () => {
     setTimeout(() => {
       colorKey[randomNumArray[6]](false)
       setMyOrYours('YOUR')
+      setDisableButtons(false)
     }, 11200)
   }
 
@@ -426,6 +434,7 @@ const GameBoard = () => {
     setTimeout(() => {
       colorKey[randomNumArray[7]](false)
       setMyOrYours('YOUR')
+      setDisableButtons(false)
     }, 12800)
   }
 
@@ -489,6 +498,7 @@ const GameBoard = () => {
     setTimeout(() => {
       colorKey[randomNumArray[8]](false)
       setMyOrYours('YOUR')
+      setDisableButtons(false)
     }, 14400)
   }
 
@@ -558,6 +568,7 @@ const GameBoard = () => {
     setTimeout(() => {
       colorKey[randomNumArray[9]](false)
       setMyOrYours('YOUR')
+      setDisableButtons(false)
     }, 16000)
   }
 
@@ -607,17 +618,17 @@ const GameBoard = () => {
 
   return (
     <>
-    <ScoreBoard roundCount={roundCount} startGame={startGame} playerClickDisplay={playerClickDisplay}/>
+    <ScoreBoard roundCount={roundCount} startGame={startGame} playerClickDisplay={playerClickDisplay} randomNumArray={randomNumArray}/>
     <div className="gameboard-outer-container">
       <div className='center-circle'>
         <p className='turn-text-1'>{myOrYours}</p>
         <p className='turn-text-2'>{turnText}</p>
       </div>
-      <div className='top-section'>
+      <div className={ disableButtons ? 'top-section disabled' : 'top-section' }>
         <div onClick={() => handlePlayerResponse(0)} className={ redActive ? 'red-box red-active' : 'red-box' }></div>
-        <div onClick={() => handlePlayerResponse(1)} className={ yellowActive ? 'yellow-box yellow-active' : 'yellow-box'}></div>
+        <div onClick={() => handlePlayerResponse(1)} className={ yellowActive ? 'yellow-box yellow-active' : 'yellow-box' }></div>
       </div>
-      <div className='bottom-section'>
+      <div className={ disableButtons ? 'bottom-section disabled' : 'bottom-section' }>
         <div onClick={() => handlePlayerResponse(2)} className={ greenActive ? 'green-box green-active' : 'green-box' }></div>
         <div onClick={() => handlePlayerResponse(3)} className={ blueActive ? 'blue-box blue-active' : 'blue-box' }></div>
       </div>
