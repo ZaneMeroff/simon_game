a7f300439b596db0df322e1c60571a51402b94fe
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-28 15:07:14 -0600

    Updates

---
+++ precache-manifest.b66370ce27a07dd74220b9268cd25f76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19828831546657f24f828ebbddb45754",
+    "url": "/simon_game/index.html"
+  },
+  {
+    "revision": "cf5593d214fc0e9a3df8",
+    "url": "/simon_game/static/css/main.61760efc.chunk.css"
+  },
+  {
+    "revision": "f403f221e846da097375",
+    "url": "/simon_game/static/js/2.7e7e74ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simon_game/static/js/2.7e7e74ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf5593d214fc0e9a3df8",
+    "url": "/simon_game/static/js/main.d584d455.chunk.js"
+  },
+  {
+    "revision": "5e2ffaa06b9d1ca439a3",
+    "url": "/simon_game/static/js/runtime-main.bbbef8f8.js"
+  }
+]);
