5941a16a49e952512a2d43a6a5d0456d3a7a3948
Zachary Nemeroff <zanemeroff@gmail.com>
2020-05-28 14:39:50 -0600

    Add test file

---
+++ src/Components/GameBoard/GameBoard.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { GameBoard } from './GameBoard';
+import { shallow } from 'enzyme';
+
+describe('GameBoard', () => {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<GameBoard />);
+  })
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('startGame', () => {
+    
+  });
+
+});
