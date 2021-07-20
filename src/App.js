47b4f1b5dafd7ca8ecc3c36fadfc4907e1ec5bfc
ZaneMeroff <zanemeroff@gmail.com>
2021-07-20 14:46:11 -0600

    fix(App.js): bug fix touchstart safari mobile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { GameBoard } from './Components/GameBoard/GameBoard';
 import './App.css';
 
 const App = () => {
+
+  useEffect(() => {
+    document.addEventListener('touchstart', () => {}, false);
+  })
+
   return (
     <div className="App">
       <h1 className='game-title-text'>Mr. Simon</h1>
